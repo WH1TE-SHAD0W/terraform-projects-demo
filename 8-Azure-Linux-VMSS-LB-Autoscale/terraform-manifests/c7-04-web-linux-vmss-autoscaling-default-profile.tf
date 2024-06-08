ad0f94cedf878d9bc17cda0440f15d8004e5b08f
D00271292 <d00271292@student.dkit.ie>
2024-06-08 21:10:49 +0100

    creating autoscaling profile

--- 8-Azure-Linux-VMSS-LB-Autoscale/terraform-manifests/c7-04-web-linux-vmss-autoscaling-default-profile.tf
+++ 8-Azure-Linux-VMSS-LB-Autoscale/terraform-manifests/c7-04-web-linux-vmss-autoscaling-default-profile.tf
@@ -51,6 +51,78 @@ resource "azurerm_monitor_autoscale_setting" "web_vmss_autoscale" {
         time_window        = "PT5M"
       }
     }
+    rule {
+      scale_action {
+        cooldown  = "PT5M"
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = 1
+      }
+      metric_trigger {
+        metric_name        = "Available Memory Bytes"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.web_vmss.id
+        operator           = "GtreaterThan"
+        statistic          = "Average"
+        threshold          = 1073741824
+        time_aggregation   = "Average"
+        time_grain         = "PT1M"
+        time_window        = "PT5M"
+      }
+    }
+    rule {
+      scale_action {
+        cooldown  = "PT5M"
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = 1
+      }
+      metric_trigger {
+        metric_name        = "Available Memory Bytes"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.web_vmss.id
+        operator           = "LessThan"
+        statistic          = "Average"
+        threshold          = 2147483648
+        time_aggregation   = "Average"
+        time_grain         = "PT1M"
+        time_window        = "PT5M"
+      }
+    }
+    rule {
+      scale_action {
+        cooldown  = "PT5M"
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = 1
+      }
+      metric_trigger {
+        metric_name        = "SYNCount"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.web_vmss.id
+        operator           = "GtreaterThan"
+        statistic          = "Average"
+        threshold          = 10
+        time_aggregation   = "Average"
+        time_grain         = "PT1M"
+        time_window        = "PT5M"
+      }
+    }
+    rule {
+      scale_action {
+        cooldown  = "PT5M"
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = 1
+      }
+      metric_trigger {
+        metric_name        = "SYNCount"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.web_vmss.id
+        operator           = "LessThan"
+        statistic          = "Average"
+        threshold          = 10
+        time_aggregation   = "Average"
+        time_grain         = "PT1M"
+        time_window        = "PT5M"
+      }
+    }
   }
 }
 
